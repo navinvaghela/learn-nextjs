9005300e0ab8ae5c6a43934ae9bc8640b57fe859
navin <v-navin.vaghela@decurtis.com>
2023-09-01 17:01:57 +0530

    sample app create for next js

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+      <div className={styles.description}>
+        <p>
+        Getting started with learning the NEXT.JS framework and setting up a deployment server VERCEL.
+        </p>
+      </div>
+
+      
+    </main>
+  )
+}
